4813edc658bbbb0f06be525b5e01f2324f7a47d8
mafeifan <mafeifan@qq.com>
2019-03-25 20:50:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello world312321\n');
+    res.send('Hello world!!\n');
 });
 
 app.listen(PORT, HOST);
